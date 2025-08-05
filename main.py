71ccd5c9f31c6b9e7132687d5d04216646806559
Justin <103222511+JustinDodemaide@users.noreply.github.com>
2025-08-04 19:57:49 -0700

    First attempt

--- main.py
+++ main.py
@@ -14,28 +14,40 @@
 # Map the .cube section to pixels - Parse each line, determine the rgb value of the line,
 # map it to a pixel on the png
 
-# Mapping:
-#slices_per_row = ceil(sqrt(N))
-#slice_x_position = b % slices_per_row
-#slice_y_position = floor(b / slices_per_row)
 
-# LUT_3D_SIZE 33
-# Line 20869: 0.186340 0.597382 0.907471
 
-# Red coord:
-# r = index % N
-# r = 20868 % 33 = 9
+from PIL import Image
+import math
 
-# Green coord:
-# g = floor(index / N) % N
-# g = floor(20868 / 33) % 33 = floor(632.36) % 33 = 632 % 33 = 5
+# get the LUT_3D_SIZE and make the blank png
+with open("Cinematic-Teal.cube", "r") as f:
+    for line in f:
+        if line.startswith("LUT_3D_SIZE"):
+            lut_size = int(line.strip().split()[1])
+            break
+    img = Image.new("RGB", (lut_size * lut_size, lut_size), (0, 0, 0))
+    # todo: if eof and no lut size, raise error
 
-# Blue coord:
-# b = floor(index / (N * N))
-# b = floor(20868 / (33 * 33)) = floor(20868 / 1089) = floor(19.16) = 19
 
-# slice_x_position = b % 6 = 19 % 6 = 1
-# slice_y_position = floor(b / 6) = floor(19 / 6) = 3
-# pixel_x = (slice_x_position * N) + r = (1 * 33) + 9 = 33 + 9 = 42
-# pixel_y = (slice_y_position * N) + g = (3 * 33) + 5 = 99 + 5 = 104
-# The color (9,5,19) is placed at (42, 104) in slice (1, 3)
+# parse the cube file and map the rgb values to pixels
+    index = 0
+    for line in f:
+        if line.strip() == "":
+            continue  # skip empty lines
+
+        r, g, b = map(float, line.strip().split())
+
+        r_index = index % lut_size
+        g_index = (index // lut_size) % lut_size
+        b_index = index // (lut_size * lut_size)
+
+        x = b_index * lut_size + r_index
+        y = g_index
+
+        index += 1
+
+        # add the color to the image
+        img.putpixel((x, y), (int(r * 255), int(g * 255), int(b * 255)))
+
+# save the image
+img.save("Cinematic-Teal.png")
